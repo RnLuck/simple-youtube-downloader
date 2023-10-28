7736c913d88b70009150cac87af2b4110f04ff29
RnLuck <2323084497@qq.com>
2023-10-28 10:29:42 +0800

    添加文档

---
+++ README.md
@@ -0,0 +1,47 @@
+# Youtube下载器，使用PowerShell打开
+## 一、简介
+本项目基于 [**yt-dlp**](https://github.com/yt-dlp/yt-dlp)，选择了几个常用的命令进行封装，主要功能为**youtube音视频和字幕下载**，其中音视频可以在不登录的情况下，下载最高画质，如4k等，字幕下载可以下载自动翻译的字幕。相比原项目更易使用。
+## 二、使用前
+### 1.安装yt-dlp与ffmpeg
+安装与配置可参考 [yt-dlp安装和使用（以Windows为例）](https://blog.csdn.net/euqlll/article/details/126905973)
+
+确保添加环境变量，可直接在powershell中输入yt-dlp、ffmpeg访问。
+```
+(base) PS D:\Files\Youtube> yt-dlp
+
+Usage: yt-dlp.exe [OPTIONS] URL [URL...]
+
+yt-dlp.exe: error: You must provide at least one URL.
+Type yt-dlp --help to see a list of all options.
+````
+```
+(base) PS D:\Files\Youtube> ffmpeg
+ffmpeg version 2023-10-26-git-2b300eb533-full_build-www.gyan.dev Copyright (c) 2000-2023 the FFmpeg developers
+  built with gcc 12.2.0 (Rev10, Built by MSYS2 project)
+  ······
+````
+### 2. 启用PowerShell脚本
+由于Windows PowerShell的默认执行策略受到限制，因此我们无法运行任何脚本，除非对脚本进行更改。 在PowerShell中使用以下命令将执行策略设置为``Unrestricted``以执行脚本。
+```
+Set-ExecutionPolicy Unrestricted
+````
+## 三、使用
+### 1.默认配置
+-目前基于本人习惯，将默认下载配置设为了``最高画质+缩略图+视频描述``
+
+-下载地址默认为``'D:/Files/Youtube/%(title)s/%(title)s.%(ext)s'``,其中，title是youtube视频的标题，ext是下载的文件格式，默认该url下所有下载的文件都放在同一个文件夹中
+
+-同时使用了``--restrict-filenames``参数，减少文件名中的空格可能会导致的bug,空格将会被替换为'_'，如``I love you``会被替换为``I_love_you``。文档中描述如下：
+>Restrict filenames to only ASCII characters,and avoid "&" and spaces in filenames
+>
+>将文件名限制为ASCII字符，避免文件名中出现"&"和空格
+
+
+-字幕格式选择了``srt``，也可自行在代码最上方更改：```$sub_fromat = 'srt'```
+
+### 2.使用示例
+
+
+
+
+
--- yt-downloader.ps1
+++ yt-downloader.ps1
@@ -1,6 +1,8 @@
 # 创建一个无限循环，允许用户多次执行操作
 $videoURL=''
 $defultLocation='D:/Files/Youtube/%(title)s/%(title)s.%(ext)s'
+$restrict_filenames = '--restrict-filenames'
+$sub_fromat = 'srt'
 while ($true) {
     if ($videoURL -eq ''){
         # 提示用户输入视频 URL
@@ -22,39 +24,40 @@ while ($true) {
     }
 
     # 提示用户选择视频保存的位置
-    $videoSaveLocation = Read-Host "请输入文件保存位置 (直接回车使用默认位置: 'D:/Files/Youtube/%(title)s/%(title)s.%(ext)s')"
+    $videoSaveLocation = Read-Host "请输入文件保存位置 (直接回车使用默认位置: $defultLocation)"
     if ([string]::IsNullOrWhiteSpace($videoSaveLocation)) {
         $videoSaveLocation = $defultLocation
     }
     
     if ($choice -eq '0') {
-        yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --write-thumbnail --write-description  --embed-metadata --merge-output-format mp4 -o $videoSaveLocation --restrict-filenames
+        # 默认配置
+        yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --write-thumbnail --write-description  --embed-metadata --merge-output-format mp4 -o $videoSaveLocation $restrict_filenames
         Write-Host "任务完成"
     } elseif ($choice -eq '1') {
         # 提示用户选择视频质量
         $formatChoice = Read-Host "请选择视频质量:`n1. 默认配置（最高画质+缩略图+视频描述）`n2. 最高质量`n3. 指定质量"
         if ($formatChoice -eq '1') {
-            yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --write-thumbnail --write-description  --embed-metadata --merge-output-format mp4 -o $videoSaveLocation --restrict-filenames
+            yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --write-thumbnail --write-description  --embed-metadata --merge-output-format mp4 -o $videoSaveLocation $restrict_filenames
         } elseif ($formatChoice -eq '2') {
-            yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --embed-metadata --merge-output-format mp4 -o $videoSaveLocation --restrict-filenames
+            yt-dlp -N 8 -f 'bv[ext=mp4]+ba[ext=m4a]' $videoURL --embed-metadata --merge-output-format mp4 -o $videoSaveLocation $restrict_filenames
         } elseif ($formatChoice -eq '3') {
             # 列出视频的可下载内容
             Write-Host "以下是视频的可下载内容："
             yt-dlp -F $videoURL
             $itemNum = Read-Host "请输入所需的视频内容序号,如 76"
-            yt-dlp -N 8 -f $itemNum $videoURL --write-thumbnail --embed-metadata -o $videoSaveLocation --restrict-filenames
+            yt-dlp -N 8 -f $itemNum $videoURL --write-thumbnail --embed-metadata -o $videoSaveLocation $restrict_filenames
         }
     } elseif ($choice -eq '2') {
         $subLangChoice = Read-Host "请选择字幕语言:`n1. 中文`n2. 英文`n3. 其他"
         if ($subLangChoice -eq '1') {
-            yt-dlp --sub-langs zh-Hans --write-auto-subs --convert-subs srt $videoURL -o $videoSaveLocation --restrict-filenames
+            yt-dlp --skip-download --sub-langs zh-Hans --write-auto-subs --convert-subs $sub_fromat $videoURL -o $videoSaveLocation $restrict_filenames
         } elseif ($subLangChoice -eq '2') {
-            yt-dlp --sub-langs en --write-auto-subs --convert-subs srt $videoURL -o $videoSaveLocation --restrict-filenames
+            yt-dlp --skip-download --sub-langs en --write-auto-subs --convert-subs $sub_fromat $videoURL -o $videoSaveLocation $restrict_filenames
         } elseif ($subLangChoice -eq '3') {
             Write-Host "以下是视频的可下载字幕："
             yt-dlp --list-subs $videoURL
             $subChoice = Read-Host "请选择要下载的字幕的 Language 名称，如 zh "
-            yt-dlp --sub-langs $subChoice --write-auto-subs --convert-subs srt $videoURL -o $videoSaveLocation --restrict-filenames
+            yt-dlp --skip-download --sub-langs $subChoice --write-auto-subs --convert-subs $sub_fromat $videoURL -o $videoSaveLocation $restrict_filenames
         }
     }
 
